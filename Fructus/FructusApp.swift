8de57f39679efaa1c7e4756c80cefd91d9ae0438
Myron Dulay <dreiohc@live.com>
2021-05-27 11:26:17 +0800

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Myron Dulay on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
