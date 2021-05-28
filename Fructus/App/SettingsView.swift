4286ce5479beb5cd7a09b028c464d9220e59f75a
Myron Dulay <dreiohc@live.com>
2021-05-28 21:37:11 +0800

    Add toggle onboarding button in settings and add ipad support

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -11,6 +11,7 @@ struct SettingsView: View {
   // MARK: - PROPERTIES
   
   @Environment(\.presentationMode) var presentationMode
+  @AppStorage("isOnboarding") var isOnboarding: Bool = false
   
   // MARK: - BODY
   var body: some View {
@@ -35,6 +36,34 @@ struct SettingsView: View {
           }
           
           // MARK: - SECTION 2
+          
+          GroupBox(label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush"), content: {
+            Divider().padding(.vertical, 4)
+            
+            Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+              .padding(.vertical, 8)
+              .frame(minHeight: 60)
+              .layoutPriority(1)
+              .font(.footnote)
+              .multilineTextAlignment(.leading)
+            
+            Toggle(isOn: $isOnboarding, label: {
+              if isOnboarding {
+                Text("restarted".uppercased())
+                  .fontWeight(.bold)
+                  .foregroundColor(.green)
+              } else {
+                Text("restart".uppercased())
+                  .fontWeight(.bold)
+                  .foregroundColor(.secondary)
+              }
+            })
+            .padding()
+            .background(
+              Color(UIColor.tertiarySystemBackground)
+                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+            )
+          })
           
           // MARK: - SECTION 3
           
